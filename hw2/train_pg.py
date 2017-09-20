f33af7bec06fa2c3f7d64c29a4ec0a3630f44d9e
MaxJiang <maxjiang93@gmail.com>
2017-09-20 14:04:32 -0700

    fix sigma bug

--- hw2/train_pg.py
+++ hw2/train_pg.py
@@ -187,8 +187,8 @@ def train_PG(exp_name='',
         sy_mean = build_mlp(sy_ob_no, ac_dim, scope="mlp_cont", n_layers=n_layers, size=size)
         sy_logstd = tf.get_variable("logstd", shape=[ac_dim])  # logstd should just be a trainable variable, not a network output.
         sy_sampled_ac = sy_mean + tf.exp(sy_logstd) * tf.random_normal(tf.shape(sy_mean))
-        sy_logprob_n = -0.5 * tf.reduce_sum(tf.multiply(tf.matmul((sy_ac_na - sy_mean),
-                                                        tf.diag(tf.square(tf.exp(sy_logstd))))
+        sy_logprob_n = -0.5 * tf.reduce_sum(tf.multiply(tf.multiply(sy_ac_na - sy_mean,
+                                                        1/tf.square(tf.exp(sy_logstd)))
                                                         , (sy_ac_na - sy_mean)), axis=-1) \
                        - tf.log(tf.sqrt((2*pi) ** ac_dim)) - 2 * tf.reduce_sum(sy_logstd)
         # Hint: Use the log probability under a multivariate gaussian.
