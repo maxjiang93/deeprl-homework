b6672619c40bc20151771aa5c9f91d306bdf0e3a
MaxJiang <maxjiang93@gmail.com>
2017-09-20 18:17:33 -0700

    yet another implem of logstd

--- hw2/train_pg.py
+++ hw2/train_pg.py
@@ -185,7 +185,7 @@ def train_PG(exp_name='',
     else:
         # YOUR_CODE_HERE
         sy_mean = build_mlp(sy_ob_no, ac_dim, scope="mlp_cont", n_layers=n_layers, size=size)
-        sy_logstd = tf.get_variable("logstd", shape=[ac_dim])  # logstd should just be a trainable variable, not a network output.
+        sy_logstd = tf.get_variable("logstd", shape=[ac_dim], initializer=tf.zeros_initializer())  # logstd should just be a trainable variable, not a network output.
         sy_sampled_ac = sy_mean + tf.exp(sy_logstd) * tf.random_normal(tf.shape(sy_mean))
         sy_logprob_n = -0.5 * tf.reduce_sum(tf.square(sy_ac_na - sy_mean) / tf.square(tf.exp(sy_logstd)), axis=-1) \
                        - ac_dim / 2 * tf.log(2 * pi) - tf.reduce_sum(sy_logstd)
